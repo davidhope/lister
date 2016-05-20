bb38205ec3eba11cd408d67f66a7bb236ab6bfcd
david hope <flav.flavor@gmail.com>
2016-05-20 16:28:13 -0500

    angular service working with url rewrite

--- js/controllers.js
+++ js/controllers.js
@@ -10,24 +10,36 @@ listingControllers.controller('ListingCtrl', ['$scope', 'ListingService',
     $scope.orderProp = 'address';
   }]);
 
-listingControllers.controller('ListingDetailCtrl', ['$scope', '$routeParams', 'ListingService',
-  function($scope, $routeParams, ListingService) {
+listingControllers.controller('ListingDetailCtrl', ['$scope', '$log', '$routeParams', 'ListingService',
+  function($scope, $log, $routeParams, ListingService) {
+
     //$routeParams.mls comes from app.js route for ListingDetailCtrl
     $scope.listing = ListingService.get({mls: $routeParams.mls}, function(listing) {
       $scope.mainImageUrl = listing.images[0];
     });
+
+    $scope.save = function(listing){
+      $log.log(listing);
+      $scope.listing.$save();
+    };
+
+    $scope.delete = function(listing){
+      $log.log(listing);
+      ListingService.delete(listing);
+    };
 
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
     };
 
     $scope.increasePrice = function() {
-      console.log($scope.listing.price);
+      $log.log($scope.listing.price);
       $scope.listing.price += 100.00;
     };
 
     $scope.decreasePrice = function() {
-      console.log($scope.price);
+      $log.log($scope.listing.price);
       $scope.listing.price -= 100.00;
     };
+
   }]);
--- js/services.js
+++ js/services.js
@@ -6,9 +6,10 @@ var listingServices = angular.module('listingServices', ['ngResource']);
 
 listingServices.factory('ListingService', ['$resource',
   function($resource){
+    /*
     return $resource('listings/:mls.json', {}, {
-      query: {method:'GET', params:{mls:'listings'}, isArray:true},
-      save: {},
-      delete{}
+      query: {method:'GET', params:{mls:'listings'}, isArray:true}
     });
+    */
+    return $resource('listings/services/listing/:id');
   }]);
