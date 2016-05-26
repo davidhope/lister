32d7b0699ce6fd3579a291018729afa2ca9d0be0
davidhope <flav.flavor@gmail.com>
2016-05-25 19:05:10 -0500

    updating index

--- includes/inc_constants.php
+++ includes/inc_constants.php
@@ -1,5 +1,6 @@
 <?php
-  $devBranch = 'C:\Users\dhope\Documents\Projects\lister';
+  //$devBranch = 'C:\Users\dhope\Documents\Projects\lister';
+  $devBranch = 'H:\Projects\lister';
 
 	define("con_userid", "userid");
   define("con_userName", "userName");
