e04f3c4d62bcb876588c97042328b03b08089988
david hope <flav.flavor@gmail.com>
2016-07-13 11:19:20 -0500

    clean up

--- includes/inc_constants.php
+++ includes/inc_constants.php
@@ -1,37 +1,14 @@
 <?php
-  $devBranch = 'C:\Apache24\htdocs\lister';
-  //$devBranch = 'H:\Projects\lister';
 
 	define("con_userid", "userid");
   define("con_userName", "userName");
 	define("con_timeout", "timeout");
 	define("con_timeoutlength", 1200);
 
-  if (strrpos($_SERVER["SERVER_SOFTWARE"],"Microsoft") === false && 1==0){
-    /*Production */
-
-    define("con_dbConn", "mysql:host=localhost;dbname=tlg");
-    define("con_dbUser","tlg_webuser");
-    define("con_dbPass","Lir5,bwiomic.");
-
-	  define("conAppRoot",$_SERVER['DOCUMENT_ROOT']);
-	  define("conClassesDir",$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'classes');
-	  define("conTemplatesDir",$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'templates');
-	  define("conIncludesDir",$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'includes');
-	  define("conSvcDir",$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .'svc');
-    define("con_TestMode",false);
-
-  }else{
-    /*Development*/
-    define("con_dbConn", "mysql:host=localhost;dbname=tlg_v2");
-    define("con_dbUser","root");
-    define("con_dbPass","ftcnj25");
-
-	  define("conAppRoot",$devBranch);
-	  define("conIncludesDir",$devBranch . DIRECTORY_SEPARATOR . 'includes');
-	  define("conClassesDir",$devBranch . DIRECTORY_SEPARATOR . 'classes');
-	  define("conSvcDir",$devBranch . DIRECTORY_SEPARATOR . 'services');
-    define("con_TestMode",true);
-  }
+  define("conAppRoot",$_SERVER['DOCUMENT_ROOT']);
+  define("conIncludesDir",$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'includes');
+  define("conClassesDir",$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'classes');
+  define("conSvcDir",$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'services');
+  define("con_TestMode",true);
 
 ?>
